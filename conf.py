cf86d209c2f66236a5c4f1e3ac9bdf13060b8b7f
Raine Nguyen <vunguyen@rubikin.com>
2013-04-24 17:20:10 +0700

    first commit

---
+++ conf.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig']
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'Nilead'
+copyright = u'2012-2013, Rubikin.com'
+version = ''
+release = ''
+exclude_patterns = []
+html_theme = 'default'
+htmlhelp_basename = 'Nileaddoc'
+man_pages = [
+    ('index', 'nilead', u'Nilead Documentation',
+     [u'Rubikin.com'], 1)
+]
